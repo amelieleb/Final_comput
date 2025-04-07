f53b52215e75fae0f5a07de3b6937f32fa884b06
AmelieIR <Amelie.Ironman-Rochon@usherbrooke.ca>
2025-04-07 15:20:11 -0400

    Enlever brouillon Jules

--- script r.R
+++ script r.R
@@ -1,36 +1,3 @@
-# modele en r
-
-model <- function(time, state, parameters) {
-  with(as.list(c(state, parameters)), {
-    dPb <-   Pb (Rb + omeg * Fe * Mp *(YBbb))   +    omeg * Fe * Mp * (YBfb) * Pf + Ib   *   (1- (Pb/Kb))
-    dPf <-   Pb (Rf + omeg * Fe * Mp *(YBff))   +    omeg * Fe * Mp * (YBbf) * Pb + If   *   (1- (Pf/Kf))                                                        
-                                                                               
-                                                                               
-                                                                               
-  }
-
-parameters <- c(
-  Rb   =
-  Rf   =
-  Mp   = 0.5
-  Fe   = fixe
-  omeg = fixe
-  Ib   =
-  If   =
-  Kb   = fixe
-  Kf   = fixe
-)
-
-state <- c(Pb = 0.01, Pf = 0.01)
-times <- seq(0, 50, by = 0.1) 
-
-
-out <- ode(y = state, times = times, func = model, parms = parameters)
-out <- as.data.frame(out)
-
-
-
-
 #FIGURE 2
 #Installer et charger les bibliothèques nécessaires
 install.packages("deSolve")
